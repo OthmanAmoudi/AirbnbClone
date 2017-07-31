0072a96780499e4d802b191f091084de3992848f
Othman Mashaab <red.fantastic@hotmail.com>
2017-08-01 01:07:01 +0300

    First Commit

---
+++ FinalAssessment/FinalAssessment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FinalAssessment
+//
+//  Created by Othman Mashaab on 01/08/2017.
+//  Copyright © 2017 Othman Mashaab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
