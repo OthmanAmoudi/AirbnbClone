17d7fa7f7b26d1d5d49abd34fefaf61dd9611e03
Othman Mashaab <red.fantastic@hotmail.com>
2017-08-04 01:22:05 +0300

    Manage booked Rooms VC, enhancments

---
+++ FinalAssessment/FinalAssessment/Booking.swift
@@ -0,0 +1,9 @@
+//
+//  Booking.swift
+//  FinalAssessment
+//
+//  Created by Othman Mashaab on 03/08/2017.
+//  Copyright © 2017 Othman Mashaab. All rights reserved.
+//
+
+import Foundation
---
+++ FinalAssessment/FinalAssessment/BookingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BookingTableViewCell.swift
+//  FinalAssessment
+//
+//  Created by Othman Mashaab on 03/08/2017.
+//  Copyright © 2017 Othman Mashaab. All rights reserved.
+//
+
+import UIKit
+
+class BookingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
